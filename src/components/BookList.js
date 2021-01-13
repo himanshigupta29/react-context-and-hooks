d09500016babd14d8b763cbeb3e6dfc9a6d73be5
himanshi <himanshigupta29@gmail.com>
2021-01-13 14:54:48 +0530

    Rewrite booklist class component to functional component and use context hooks instead of context api

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,25 +1,21 @@
-import React, { Component } from 'react'
+import React, { useContext } from 'react'
 import {ThemeContext} from '../contexts/ThemeContext';
 
-class BookList extends Component {
+const BookList = () => {
 
-    static contextType = ThemeContext;
-    render() {
+    const {isLightTheme, light, dark} = useContext(ThemeContext);
 
-        const {isLightTheme, light, dark} = this.context;
+    const theme = isLightTheme ? light : dark;
 
-        const theme = isLightTheme ? light : dark;
-
-        return (
-            <div className="book-list" style = {{ background: theme.bg, color: theme.syntax }}>
-                <ul>
-                    <li style = {{background: theme.ui}}>the way of kings</li>
-                    <li style = {{background: theme.ui}}>the name of the wind</li>
-                    <li style = {{background: theme.ui}}>the final empire</li>
-                </ul>
-            </div>
-        )
-    }
+    return (
+        <div className="book-list" style = {{ background: theme.bg, color: theme.syntax }}>
+        <ul>
+            <li style = {{background: theme.ui}}>the way of kings</li>
+            <li style = {{background: theme.ui}}>the name of the wind</li>
+            <li style = {{background: theme.ui}}>the final empire</li>
+        </ul>
+    </div>
+    );
 }
 
 export default BookList;
